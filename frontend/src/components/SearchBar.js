6b7d0c2027f39809e683515f88091a81e99c7929
Igor Fedotov <igor.fedotov@brickmakers.de>
2017-12-13 21:39:52 +0100

    frontend added

---
+++ frontend/src/components/SearchBar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import TextField from 'material-ui/TextField';
+
+
+export default class SearchBar extends Component {
+    
+    render () {
+        return (
+            <TextField
+                
+            />
+        );
+    }
+}
+
+const searchBarDefaultStyle = {
+   
+}
