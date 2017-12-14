b185083888b8cde91aac212927b58237a53ec927
Igor Fedotov <igor.fedotov@brickmakers.de>
2017-12-14 17:32:20 +0100

    [Frontend] added submit button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import SearchBar from './components/SearchBar';
 import ImageGrid from './components/ImageGrid';
+import { Button } from 'material-ui'
 
-class App extends Component {
+export default class App extends Component {
 
-  
+  onSubmitClicked = (event) => {
+    console.log('Submit clicked')
+  }
 
   render() {
   
@@ -15,15 +18,26 @@ class App extends Component {
     }
 
     return (
-      <div className="App" style={appStyle}>
+      <div className="App" style={appStyles.app}>
         <SearchBar onClick={() => console.log('search tapped')} />
         <ImageGrid imageData={imagedata} style={{ marginTop: 48 }}/>
+        <Button raised style={appStyles.submitButton} onClick={event => this.onSubmitClicked(event)}>Submit</Button>
       </div>
     );
   }
 }
-const appStyle = {
-  padding: 0,
-  margin: 12
+const appStyles = {
+  app: {
+    padding: 0,
+    margin: 12
+  },
+  submitButton: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 48, 
+    marginBottom: 48, 
+    display: 'block', 
+    backgroundColor: 'rgb(66,133,244)',
+    color: 'white'
+    }
 }
-export default App;
