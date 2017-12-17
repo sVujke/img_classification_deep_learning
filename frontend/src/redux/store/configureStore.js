5226144804d49133094f029744a1a070125f63f9
Igor Fedotov <igor.fedotov@brickmakers.de>
2017-12-17 23:50:33 +0100

    [Frontend] [NPM] redux saga, redux logger

--- frontend/src/redux/store/configureStore.js
+++ frontend/src/redux/store/configureStore.js
@@ -2,12 +2,14 @@ import { createStore, applyMiddleware } from 'redux'
 import rootReducer from '../reducers'
 import createSagaMiddleware from 'redux-saga'
 import sagas from '../sagas'
+import createLogger from 'redux-logger'
 
 export default function configureStore(initialState) {
 
     const sagaMiddleware = createSagaMiddleware();
+    const logger = createLogger();
 
-    const store = createStore(rootReducer, initialState, applyMiddleware(sagaMiddleware))
+    const store = createStore(rootReducer, initialState, applyMiddleware(sagaMiddleware, logger))
     sagaMiddleware.run(sagas);
 
     if (module.hot) {
