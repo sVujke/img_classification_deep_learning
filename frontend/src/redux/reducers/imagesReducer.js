3f52350143ef88d1cfa9ac39c5243595ee7b014c
Igor Fedotov <igor.fedotov@brickmakers.de>
2018-02-03 14:26:25 +0100

    [Fronend] Fixed a typo with posting

--- frontend/src/redux/reducers/imagesReducer.js
+++ frontend/src/redux/reducers/imagesReducer.js
@@ -64,7 +64,7 @@ export default function (state = initialState, action) {
 
         case POST_FEEDBACK_SUCCESS:
             {
-                return { ...initialState, loading: false };
+                return { ...initialState, loading: false, posted: true };
             }
 
         case POST_FEEDBACK_FAILURE:
