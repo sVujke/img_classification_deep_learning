018e14cd3221d3b74c882b8ceb4216de7f6eb83e
sVujke <stefanvujovic93@gmail.com>
2018-03-25 23:00:19 +0200

    added method docs

--- dlsite/dlsite/website/search_history.py
+++ dlsite/dlsite/website/search_history.py
@@ -11,6 +11,9 @@ class SearchHistory():
         # self.df_history = self.read_history()
 
     def read_history(self):
+        """ Reads file or creates one if it does not exist
+        :return: DataFrame loaded from csv 
+        """
         if path.isfile(self.HISTORY_FILE_PATH):
             return pd.read_csv(self.HISTORY_FILE_PATH)
 
@@ -19,6 +22,10 @@ class SearchHistory():
         return df
 
     def search_history_update(self, query):
+        """ If query has already been recorded increments number of searches, 
+        if not, adds new query with 1 searches
+        :param query: query submited by user
+        """
         df = self.read_history()
 
         if query not in df["query"].values:
@@ -30,6 +37,10 @@ class SearchHistory():
         df.to_csv(self.HISTORY_FILE_PATH, index=False)
 
     def get_number_of_searches(self, query):
+        """
+        :param query: query that user submited 
+        :return: number of searches so far for the given query
+        """
         df = self.read_history()
         if query not in df["query"].values:
             return 0
